801e0ba7d89c3299a625a35de02fb3c5cc441f3b
RickvdHeijden <76222429+RickvdHeijden@users.noreply.github.com>
2022-06-15 15:39:51 +0200

    Implemented a way to sniff till we find what we want to find.

--- authentication-md5/readStream.py
+++ authentication-md5/readStream.py
@@ -1,11 +1,33 @@
+from threading import Thread
 from scapy.all import *
-from scapy.layers.inet import UDP
+from scapy.layers.inet import TCP
 from scapy.layers.rtp import *
-# len <= 390 and len >= 370 and
-capture = sniff(filter="len >= 370 && src host 192.168.1.3 && dst host 192.168.1.108 && tcp dst port 554",
-                iface=conf.iface, count=30)
-capture.summary()
-wrpcap("GfG.pcap", capture)
+
+
+def handle_packet(pkt):
+    # pkt.show()
+    payload = pkt[TCP].payload
+    # print('joe joe')
+    print(payload)
+    sys.exit()
+    # if pkt.__contains__('rtsp://'):
+    #     print(pkt)
+
+
+def sniff_function():
+    capture = sniff(filter="len >= 370 && src host 192.168.1.3 && dst host 192.168.1.108 && tcp dst port 554",
+                    iface=conf.iface, prn=handle_packet)
+    capture.summary()
+
+
+thread = Thread(target=sniff_function)
+thread.start()
+thread.join()
+
+print("joe joe")
+
+# capture.summary()
+# wrpcap("GfG.pcap", capture)
 
 # bind_layers(UDP, RTP, dport=5016)
 # pkts = sniff(offline="GfG.pcap")  # , filter="src host 192.168.1.4 and dst host 192.168.1.108")
